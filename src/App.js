f621aba132aa0f34821f298140a109a918a2aed2
omansh22 <lavania.omansh@gmail.com>
2020-06-13 00:33:09 +0530

    webinar merge

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import WebninarHome from './pages/webinardetails/webinardetails'
+import WebinarMain from './pages/webinarmain/webinar'
+
+class App extends Component {
+  render(){
+  return (
+    
+    <WebinarMain/>
+    //<WebninarHome/> //webinar description component
+  );
+  }
+}
+
+export default App;
